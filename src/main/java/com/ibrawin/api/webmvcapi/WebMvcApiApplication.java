d70f6110538d3b4a33e1380f9552c9ae5996584f
Ibrahim Ayanbunmi <ibrahim-ayanbunmi@live.co.uk>
2020-11-30 09:35:46 +0000

    First commit

---
+++ src/main/java/com/ibrawin/api/webmvcapi/WebMvcApiApplication.java
@@ -0,0 +1,13 @@
+package com.ibrawin.api.webmvcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMvcApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibrawin/api/webmvcapi/WebMvcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrawin.api.webmvcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMvcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
