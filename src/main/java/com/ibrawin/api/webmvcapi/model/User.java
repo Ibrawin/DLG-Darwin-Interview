e9f4ebc42b95f83b959ce7a201fff0a394fca720
Ibrahim Ayanbunmi <ibrahim-ayanbunmi@live.co.uk>
2020-11-30 10:48:22 +0000

    Created pojo model and annotated it so it can be persisted as a JPA entity

---
+++ src/main/java/com/ibrawin/api/webmvcapi/model/User.java
@@ -0,0 +1,30 @@
+package com.ibrawin.api.webmvcapi.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+
+    private String email;
+
+    private String password;
+
+    private String phoneNumber;
+
+    private String department;
+
+    private String jobTitle;
+
+}
